8f91dd02e6fc841f7630b16dfe649bbdc10b168d
jS <js@email.com>
2025-09-18 16:46:38 -0700

    prettier formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <main className="flex justify-center items-center h-screen">
-        <Image
-          className="dark:invert"
-          src="/logo.png"
-          alt="JavaScript logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main> 
+    <main className="flex h-screen items-center justify-center">
+      <Image
+        className="dark:invert"
+        src="/logo.png"
+        alt="JavaScript logo"
+        width={180}
+        height={38}
+        priority
+      />
+    </main>
   );
 }
